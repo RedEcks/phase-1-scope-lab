e4dad4ef82f74fe242044817ad97a380dc43edca
Xavier Dunkley <xavierxv18@gmail.com>
2022-02-09 12:38:42 -0500

    commit tree 94388376f8dec2c4fdf57d1ccf3f353c9997fda7

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "jim";
+
+function upperCaseCustomerName(){
+    customerName= customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    window.bestCustomer="not bob";
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="bob";
+}
